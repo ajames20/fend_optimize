9fd4e603616276510e4534e4cb64c3673d4716cb
Andrew James <ajames20@gmail.com>
2017-06-29 21:38:26 +0300

    remove func

--- src/views/js/main.js
+++ src/views/js/main.js
@@ -1216,31 +1216,6 @@ var resizePizzas = function(size) {
 
   changeSliderLabel(size);
 
-  function determineDx(elem, size) {
-    var oldWidth = elem.offsetWidth;
-    var windowWidth = document.querySelector('#randomPizzas').offsetWidth;
-    var oldSize = oldWidth / windowWidth;
-
-    // Changes the slider value to a percent width
-    function sizeSwitcher(size) {
-      switch (size) {
-        case '1':
-          return 0.25;
-        case '2':
-          return 0.3333;
-        case '3':
-          return 0.5;
-        default:
-          console.log('bug in sizeSwitcher');
-      }
-    }
-
-    var newSize = sizeSwitcher(size);
-    var dx = (newSize - oldSize) * windowWidth;
-
-    return dx;
-  }
-
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
     var newWidth;
